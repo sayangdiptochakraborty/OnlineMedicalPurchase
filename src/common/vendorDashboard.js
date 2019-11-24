a9717989489727d6e604e41eb2ba37c67445fdf3
Sayangdipto Chakraborty <sayangdipto.chakraborty556@gmail.com>
2019-11-24 23:39:17 +0530

    Vendor Dashboard

--- src/common/vendorDashboard.js
+++ src/common/vendorDashboard.js
@@ -6,6 +6,12 @@ import { Card, Icon, Button, Input, Upload, Divider } from 'antd';
 import { List, Avatar} from 'antd';
 import Header from './header';
 import Footer from './footer';
+import * as firebase from 'firebase';
+import firebaseConfig from './firebaseConfig';
+import 'firebase/auth';
+import 'firebase/database';
+import 'antd/dist/antd.css';
+import { Empty,message } from 'antd';
 
 const listData = [];
 for (let i = 0; i < 6; i++) {
@@ -38,16 +44,96 @@ export default class VendorDashboard extends React.Component
             addMedicine: false,
             itemList: false,
             orderList: false,
+            name: '',
+            address: '',
+            phone: '',
+            email: '',
+            transaction: [],
+            image: '',
+            thumb_image: '',
+            username: '',
+            itemName: '',
+            itemPrice: '',
+            itemExpiry: '',
+            itemType: 'tablet',
+            itemUsage: 'Medicine'
         }
+        if(!firebase.apps.length)
+        {
+          firebase.initializeApp(firebaseConfig);
+        }
+        this.update =  this.update.bind(this)
+        this.addItem = this.addItem.bind(this)
+    }
+    addItem = (e) => {
+      var meds = JSON.parse(sessionStorage.getItem('meds'))
+      if(meds[this.state.name]!=null)
+      {
+        message.loading('Adding Item')
+        firebase.database().ref().child(`Medicine/${this.state.itemName}/Shop`).child(this.state.name).set({
+          Expiry: this.state.itemExpiry,
+          Name: this.state.name,
+          image: this.state.image
+        }).then(()=>{message.success('Item Added');setTimeout(()=>{document.location.reload();},1500)}).catch(()=>{
+          message.error('Failed')
+        });
+      }
+      else
+      {
+        firebase.database().ref().child(`Medicine/${this.state.itemName}`).set({
+          Info: {
+            Name: this.state.itemName,
+            Price: this.state.itemPrice,
+            Type: this.state.itemType,
+            usage: this.state.itemUsage
+          },
+        }).then(()=>{
+          message.loading('Adding Item')
+        }).catch(()=>{
+          message.error('Failed')
+        })
+        setTimeout(()=>{
+        firebase.database().ref().child(`Medicine/${this.state.itemName}/Shop/${this.state.name}`).set({
+          Expiry: this.state.itemExpiry,
+          Name: this.state.name,
+          image: this.state.image
+        }).then(()=>{message.success('Item Added');setTimeout(()=>{document.location.reload();},800)}).catch(()=>{
+          message.error('Failed')
+        })},2000)
+      }
+    }
+    update = (e) => {
+      var uid = sessionStorage.getItem('uid');
+        firebase.database().ref().child(`Seller/${uid}`).set({
+          Shop_Address: this.state.address,
+          Shop_Phone: this.state.phone,
+          Shop_Name: this.state.name,
+          email: this.state.email,
+          username: this.state.username,
+          image: this.state.image,
+          thumb_image: this.state.thumb_image,
+          Transaction: this.state.transaction
+        }).then(()=>{
+          message.success('Details Updated')
+          sessionStorage.removeItem('userDetails');
+          var dbRef = firebase.database().ref().child('Seller').child(uid);
+          dbRef.on('value',snap=>sessionStorage.setItem('userDetails',JSON.stringify(snap.val())));
+          document.location.reload();
+        }).catch(function(error){
+          message.log(error.message);
+        })
+    }
+    async componentWillMount()
+    {
+      var uid = sessionStorage.getItem('userDetails')
+      if(uid!=undefined)
+      {
+        var userDetails = JSON.parse(sessionStorage.getItem('userDetails'))
+        this.setState({name: userDetails.Shop_Name, address: userDetails.Shop_Address, phone: userDetails.Shop_Phone, transaction: userDetails.Transaction, image:userDetails.image, thumb_image: userDetails.thumb_image,username: userDetails.username})
+      }
     }
     render()
     {
-        const uploadButton = (
-            <div>
-              <Icon type={this.state.loading ? 'loading' : 'plus'} />
-              <div className="ant-upload-text">Upload</div>
-            </div>
-          );
         return(
             <div>
               <Header/>
@@ -72,68 +158,52 @@ export default class VendorDashboard extends React.Component
                     <Divider/>{
                         this.state.settings?
                         <div>
-                        <Row type="flex" justify="space-around">
-                        <Col span={7}></Col>
-                        <Col span={6}>
-                            <Upload className="avatar-uploader" listType="picture-card" showUploadList={false}>{uploadButton}</Upload>
-                        </Col>
-                        <Col span={3}></Col>
+                    <br/>
+                    <Row type="flex" justify="space-around">
+                        <Col span={8}><Input addonBefore={<Icon type="user" />}  defaultValue={this.state.name} onChange={(e)=>{this.setState({name:e.target.value})}} size="large"/></Col>
+                        <Col span={8}><Input disabled="true" addonBefore={<Icon type="mail" />}  placeholder={this.state.email} size="large"/></Col>
                     </Row>
                     <br/>
                     <Row type="flex" justify="space-around">
-                        <Col span={8}><Input addonBefore={<Icon type="user" />} size="large"/></Col>
-                        <Col span={8}><Input disabled="true" addonBefore={<Icon type="mail" />} size="large"/></Col>
+                        <Col span={8}><Input addonBefore={<Icon type="phone" />}  defaultValue={this.state.phone} onChange={(e)=>{this.setState({phone:e.target.value})}} size="large"/></Col>
+                        <Col span={8}><Input addonBefore={<Icon type="environment" />}  defaultValue={this.state.address} onChange={(e)=>{this.setState({address:e.target.value})}} size="large"/></Col>
                     </Row>
                     <br/>
                     <Row type="flex" justify="space-around">
-                        <Col span={8}><Input addonBefore={<Icon type="phone" />} size="large"/></Col>
-                        <Col span={8}><Input addonBefore={<Icon type="environment" />} size="large"/></Col>
-                    </Row>
-                    <br/>
-                    <Row type="flex" justify="space-around">
-                        <Col span={8}><Input.Password addonBefore={<Icon type="lock" />} size="large"/></Col>
-                        <Col span={8}><Input.Password addonBefore={<Icon type="lock" />} size="large"/></Col>
-                    </Row>
-                    <br/>
-                    <Row type="flex" justify="space-around">
-                        <Col span={12}><Input.Password addonBefore={<Icon type="lock" />} size="large"/></Col>
-                    </Row>
-                    <br/>
-                    <Row type="flex" justify="space-around">
-                        <Col span={12}><Button type="primary" block>Update</Button></Col>
+                        <Col span={12}><Button type="primary" block onClick={this.update}>Update</Button></Col>
                     </Row>
                     </div>:null
                     }
                     {
                         this.state.addMedicine?<div>
                             <Row type="flex" justify="space-around">
-                                <Col span={7}></Col>
-                                <Col span={6}>
-                                    <Upload className="avatar-uploader" listType="picture-card" showUploadList={false}>{uploadButton}</Upload>
-                                </Col>
-                                <Col span={3}></Col>
-                            </Row>
-                            <br/>
-                            <Row type="flex" justify="space-around">
                                 <Col span={3}></Col>
                                 <Col span={8} className="medDetail">
-                                    <Input addonBefore={<span style={{width:'200px'}}>Item Name</span>} size="large"/>
+                                    <Input addonBefore={<span style={{width:'200px'}}>Item Name</span>} value={this.state.itemName} onChange={(e)=>{this.setState({itemName:e.target.value})}} size="large"/>
                                     <br/>
                                     <br/>
-                                    <Input addonBefore={<span style={{width:'200px'}}>Item Price</span>} size="large"/>
+                                    <Input addonBefore={<span style={{width:'200px'}}>Item Price</span>} value={this.state.itemPrice} onChange={(e)=>{this.setState({itemPrice:e.target.value})}} size="large"/>
                                     <br/>
                                     <br/>
-                                    <Input addonBefore={<span style={{width:'200px'}}>Item Qty.</span>} size="large"/>
+                                    <Input addonBefore={<span style={{width:'200px'}}>Best Before</span>} value={this.state.itemExpiry} onChange={(e)=>{this.setState({itemExpiry:e.target.value})}} size="large"/>
                                     <br/>
                                     <br/>
-                                    <Radio.Group defaultValue="Medicine" size="large">
+                                    <Radio.Group defaultValue={this.state.itemUsage} onChange={(e)=>{this.setState({itemUsage:e.target.value})}}size="large">
                                         <Radio.Button value="Medicine">Medicine</Radio.Button>
                                         <Radio.Button value="Supplement">Supplement</Radio.Button>
                                         <Radio.Button value="Diet">Diet</Radio.Button>
                                     </Radio.Group>
                                     <br/>
                                     <br/>
-                                    <Button type="primary" block>Add Item</Button>
+                                    <Row type="flex" justify="space-around">
+                                      <Radio.Group defaultValue={this.state.itemType} onChange={(e)=>{this.setState({itemType:e.target.value})}}size="large">
+                                        <Radio.Button value="tablet">Tablet</Radio.Button>
+                                        <Radio.Button value="syrup">Syrup</Radio.Button>
+                                      </Radio.Group>
+                                    </Row>
+                                    <br/>
+                                    <br/>
+                                    <Button type="primary" block onClick={this.addItem}>Add Item</Button>
                                 </Col>
                                 <Col span={3}></Col>
                             </Row>
